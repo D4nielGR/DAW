0c2a3bb00de881ad7f491f29ba60a14bfdb1319f
Monkey D. Luffy <dguerei620@g.educaand.es>
2024-02-23 14:22:14 +0100

    First practice with docker after the tutorial

---
+++ 01_BloqueDocker&AWS/02PracticeDocker/P02PractiseAfterTutorialDocker/P02Docker/php/DressFruits/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>DressFruits</title>
+</head>
+<body>
+    <header>
+        <div class="Navbar">
+            <div>   <a href="index.php">  <p>Show List</p>  </a>    </div>
+            <div>   <a href="insert.php"> <p>  Insert </p>  </a>    </div>
+            <div>   <a href="modify.php"> <p>  Modify </p>  </a>    </div>
+            <div>   <a href="delete.php"> <p>  Delete </p>  </a>    </div>
+        </div>
+    </header>
+
+    <body>
+        <?php
+            echo "Bienvenido a mi tienda, DressFruits. Disfruta de los productos que encontrarás aquí";
+        ?>
+    </body>
+</body>
+</html>
